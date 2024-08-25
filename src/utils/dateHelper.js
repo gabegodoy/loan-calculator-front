b5407a5548baf067a23d879cc15879a80733a005
gabegodoy <gabe.profe@gmail.com>
2024-08-25 17:54:30 -0300

    Refactor: :recycle: Change Data Type To Java Date

--- src/utils/dateHelper.js
+++ src/utils/dateHelper.js
@@ -3,7 +3,13 @@ function stringToDate(dateString) {
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Meses são base 0
     const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
+    return `${year}-${month}-${day}`;
 }
 
-export { stringToDate };
+function formatDate(dateString) {
+    const [year, month, day] = dateString.split('-');
+    return `${day}/${month}/${year}`;
+  }
+
+export { stringToDate, formatDate };
+
