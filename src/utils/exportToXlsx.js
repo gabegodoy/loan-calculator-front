48073ea821913b346a47ed6b028189e0e80d3349
gabegodoy <gabe.profe@gmail.com>
2024-08-26 01:21:39 -0300

    Feat: :sparkles: Implement Export To Excel

---
+++ src/utils/exportToXlsx.js
@@ -0,0 +1,30 @@
+import { Workbook } from "exceljs";
+
+async function exportToXlsx(data, headers, fileName) {
+    const workbook = new Workbook();
+    const worksheet = workbook.addWorksheet("Resultado de Cálculo de Empréstimo");
+
+    worksheet.addRow(headers);
+
+    data.forEach((element) => {
+        console.log(data)
+        console.log(element)
+        worksheet.addRow(element);
+    });
+
+    workbook.xlsx.writeBuffer().then((data) => {
+        const blob = new Blob([data], {
+            type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        });
+        const csvURL = window.URL.createObjectURL(blob);
+        const tempLink = document.createElement("a");
+        tempLink.href = csvURL;
+        tempLink.setAttribute(
+            "download",
+            fileName + "-" + new Date().toISOString().split("T")[0] + ".xlsx",
+        );
+        tempLink.click();
+    });
+}
+
+export { exportToXlsx };
