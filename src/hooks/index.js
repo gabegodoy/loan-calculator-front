de56f5364c25fe70708e52c538810b6096642da9
gabegodoy <gabe.profe@gmail.com>
2024-08-29 08:11:16 -0300

    Chore: :truck: Remove Ngrok route

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const useAxios = () => {
 
     const instance = axios.create({
-        baseURL: import.meta.env.VITE_API_URL || "http://localhost:8085/api/v1/",
+        baseURL: import.meta.env.VITE_API_URL,
     });
     
     return { axios: instance };
